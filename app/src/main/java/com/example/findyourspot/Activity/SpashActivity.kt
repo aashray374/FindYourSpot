f6af6b09d2d0f343b74ffff5ce3f891f14c3f757
aashray374 <aashraymahajan374@gmail.com>
2024-04-07 19:06:13 +0530

    Commit 5 Made Changes in layout

--- app/src/main/java/com/example/findyourspot/Activity/SpashActivity.kt
+++ app/src/main/java/com/example/findyourspot/Activity/SpashActivity.kt
@@ -6,15 +6,17 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import com.example.findyourspot.R
+import com.example.findyourspot.databinding.ActivitySpashBinding
 
 class SpashActivity : AppCompatActivity() {
+    private lateinit var binding:ActivitySpashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_spash)
+        binding=ActivitySpashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        Handler(Looper.getMainLooper()).postDelayed({
+        binding.go.setOnClickListener {
             startActivity(Intent(this,DetailActivity::class.java))
-            finish()
-        },3000)
+        }
     }
 }
